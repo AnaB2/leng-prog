4af54292307aa1525fccebc7fa7b52736df4b339
AnaB2 <ana.borda@ing.austral.edu.ar>
2024-08-05 13:21:13 -0300

    Added CollatzConjecture.hs

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,14 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz number
+    | number <= 0 = Nothing  -- Devuelve `Nothing` si el número es menor o igual a 0
+    | otherwise   = Just (collatzHelper number 0)
+
+collatzHelper :: Integer -> Integer -> Integer
+collatzHelper 1 count = count
+collatzHelper number count
+    | even number = collatzHelper (number `div` 2) (count + 1)
+    | otherwise   = collatzHelper (number * 3 + 1) (count + 1)
+
+
